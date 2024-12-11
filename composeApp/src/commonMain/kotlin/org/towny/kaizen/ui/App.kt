a00e944bb85b0fc074ae7ccb25524474da79b93c
AnthonyMde <anthony.mamode@gmail.com>
2024-12-11 18:39:47 +0100

    Use material3 instead of material + setup theme colors.

--- composeApp/src/commonMain/kotlin/org/towny/kaizen/ui/App.kt
+++ composeApp/src/commonMain/kotlin/org/towny/kaizen/ui/App.kt
@@ -1,10 +1,11 @@
 package org.towny.kaizen.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -12,18 +13,23 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.towny.kaizen.ui.theme.AppTheme
 import org.towny.kaizen.utils.DateUtils
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+    AppTheme {
+        Column(
+            Modifier.fillMaxSize().background(color = MaterialTheme.colorScheme.background),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text(
                 text = DateUtils.getTodaysDate(),
                 modifier = Modifier.padding(20.dp),
                 textAlign = TextAlign.Center,
-                fontSize = 24.sp
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.primary
             )
         }
     }
