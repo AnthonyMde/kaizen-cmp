f9f0c732698eeab9a38ede57221f38a84044044b
AnthonyMde <anthony.mamode@gmail.com>
2024-12-29 18:04:13 +0100

    Use GetUserSessionUseCase in ChallengesService.

--- composeApp/src/commonMain/kotlin/org/towny/kaizen/domain/services/ChallengesService.kt
+++ composeApp/src/commonMain/kotlin/org/towny/kaizen/domain/services/ChallengesService.kt
@@ -1,21 +1,18 @@
 package org.towny.kaizen.domain.services
 
-import kotlinx.coroutines.flow.first
-import org.towny.kaizen.domain.models.Resource
 import org.towny.kaizen.domain.repository.ChallengesRepository
-import org.towny.kaizen.domain.repository.UsersRepository
 
 class ChallengesService(
     private val challengesRepository: ChallengesRepository,
-    private val usersRepository: UsersRepository
+    private val getUserSessionUseCase: GetUserSessionUseCase
 ) {
     suspend fun toggleChallenge(
         userId: String,
         challengeId: String,
         isChecked: Boolean
     ) {
-        val userSession = usersRepository.getSavedUserSession().first()
-        if (userSession !is Resource.Success || userSession.data == null || userId != userSession.data) {
+        val userSession = getUserSessionUseCase()
+        if (userSession == null || userId != userSession) {
             return
         } else {
             challengesRepository.toggleChallenge(userId, challengeId, isChecked)
