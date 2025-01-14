fcbdd9203c7de8f2eb3f7fa91f2aab7fb4e87a99
AnthonyMde <anthony.mamode@gmail.com>
2025-01-15 00:02:44 +0100

    Proper padding for send friend request loader.

--- composeApp/src/commonMain/kotlin/org/towny/kaizen/ui/screens/my_friends/components/FriendPreview.kt
+++ composeApp/src/commonMain/kotlin/org/towny/kaizen/ui/screens/my_friends/components/FriendPreview.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Send
@@ -69,6 +70,7 @@ fun FriendPreview(
 
         if (isLoading) {
             CircularProgressIndicator(modifier = Modifier.size(24.dp), strokeWidth = (2.dp))
+            Spacer(modifier = Modifier.width(0.dp)) // Hack to avoid miscalculation with IconButton.
         } else {
             IconButton(
                 onClick = {
