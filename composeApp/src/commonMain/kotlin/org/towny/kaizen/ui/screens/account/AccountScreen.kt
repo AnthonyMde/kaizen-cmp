4cc29915740d3eded589bafeaa4a588f09e79649
AnthonyMde <anthony.mamode@gmail.com>
2025-01-15 17:44:41 +0100

    Add delete account button.

--- composeApp/src/commonMain/kotlin/org/towny/kaizen/ui/screens/account/AccountScreen.kt
+++ composeApp/src/commonMain/kotlin/org/towny/kaizen/ui/screens/account/AccountScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -19,6 +21,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -183,6 +186,23 @@ fun AccountScreen(
                     description = "My friends.",
                     modifier = Modifier.padding(top = 8.dp)
                 )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                TextButton(
+                    onClick = {},
+                    content = {
+                        Text(
+                            "Delete my account",
+                            color = MaterialTheme.colorScheme.error,
+                            modifier = Modifier
+                        )
+                    },
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
             }
         }
     }
