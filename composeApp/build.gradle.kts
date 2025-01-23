78133aea7be46979d22b1a4206b58d449351f882
AnthonyMde <anthony.mamode@gmail.com>
2025-01-23 16:33:01 +0100

    Bump version to 1.0.1

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -92,8 +92,8 @@ android {
         applicationId = "com.makapp.kaizen"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
     packaging {
         resources {
