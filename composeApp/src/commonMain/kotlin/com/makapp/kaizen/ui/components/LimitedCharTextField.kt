4c78635d0606eebb3f9390d25a3854249b7966d3
AnthonyMde <anthony.mamode@gmail.com>
2025-03-10 18:33:14 +0100

    Add LimitedCharTextField component.

---
+++ composeApp/src/commonMain/kotlin/com/makapp/kaizen/ui/components/LimitedCharTextField.kt
@@ -0,0 +1,102 @@
+package com.makapp.kaizen.ui.components
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import com.makapp.kaizen.ui.screens.components.FormErrorText
+
+@Composable
+fun LimitedCharTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    maxCharAllowed: Int,
+    textError: String?,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    prefix: @Composable (() -> Unit)? = null,
+    suffix: @Composable (() -> Unit)? = null,
+    supportingText: @Composable (() -> Unit)? = null,
+    isError: Boolean = false,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = false,
+    maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
+    minLines: Int = 1,
+    interactionSource: MutableInteractionSource? = null,
+    shape: Shape = OutlinedTextFieldDefaults.shape,
+    colors: TextFieldColors = OutlinedTextFieldDefaults.colors()
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        OutlinedTextField(
+            value,
+            onValueChange,
+            modifier,
+            enabled,
+            readOnly,
+            textStyle,
+            label,
+            placeholder,
+            leadingIcon,
+            trailingIcon,
+            prefix,
+            suffix,
+            supportingText,
+            isError,
+            visualTransformation,
+            keyboardOptions,
+            keyboardActions,
+            singleLine,
+            maxLines,
+            minLines,
+            interactionSource,
+            shape,
+            colors
+        )
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            FormErrorText(
+                textError ?: "",
+                modifier = Modifier
+                    .padding(
+                        start = 8.dp, end = 8.dp,
+                        top = if (textError != null) 4.dp else 0.dp,
+                        bottom = if (textError != null) 4.dp else 0.dp
+                    )
+                    .weight(1f)
+            )
+
+            Text(
+                "${value.length}/$maxCharAllowed",
+                style = MaterialTheme.typography.labelMedium,
+                modifier = Modifier
+                    .padding(end = 8.dp)
+            )
+        }
+    }
+}
