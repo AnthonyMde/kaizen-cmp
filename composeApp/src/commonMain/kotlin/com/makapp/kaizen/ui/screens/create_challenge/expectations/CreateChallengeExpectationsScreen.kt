332ad2e1c7730189693bc2e41248742ac7387478
AnthonyMde <anthony.mamode@gmail.com>
2025-03-13 16:13:37 +0100

    [fix] Make expectations screen compatible with softkeyboard.

--- composeApp/src/commonMain/kotlin/com/makapp/kaizen/ui/screens/create_challenge/expectations/CreateChallengeExpectationsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/makapp/kaizen/ui/screens/create_challenge/expectations/CreateChallengeExpectationsScreen.kt
@@ -4,12 +4,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -23,7 +27,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.SoftwareKeyboardController
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -86,6 +89,7 @@ fun CreateChallengeExpectationsScreen(
     navArgs: ChallengeExpectationsNavArgs
 ) {
     val keyboard = LocalSoftwareKeyboardController.current
+    val scrollState = rememberScrollState()
 
     Scaffold(
         containerColor = MaterialTheme.colorScheme.surface,
@@ -99,89 +103,82 @@ fun CreateChallengeExpectationsScreen(
             )
         },
     ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .padding(24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+            .imePadding()
         ) {
-            Box(
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .clip(RoundedCornerShape(16.dp))
-                    .background(MaterialTheme.colorScheme.tertiaryContainer)
-                    .padding(16.dp)
+                    .fillMaxHeight()
+                    .verticalScroll(scrollState)
+                    .padding(24.dp)
             ) {
-                Text(
-                    text = "Kaizen is about growth, not just goals \uD83E\uDDD8\u200D♂\uFE0F Where do you want to be in 365 days? \nWrite it down - it’s your first step forward! \uD83D\uDE80",
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            // Commitment Field.
-            LimitedCharTextField(
-                onValueChange = { expectations ->
-                    onAction(
-                        CreateChallengeExpectationsAction.OnExpectationsValueChange(expectations)
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(MaterialTheme.colorScheme.tertiaryContainer)
+                        .padding(16.dp)
+                ) {
+                    Text(
+                        text = "Kaizen is about growth, not just goals \uD83E\uDDD8\u200D♂\uFE0F Where do you want to be in 365 days? \nWrite it down - it’s your first step forward! \uD83D\uDE80",
+                        style = MaterialTheme.typography.bodyMedium
                     )
-                },
-                value = state.expectationsInputValue,
-                maxCharAllowed = CreateChallengeViewModel.MAX_CHALLENGE_EXPECTATIONS_LENGTH,
-                textError = null,
-                shape = RoundedCornerShape(16.dp),
-                placeholder = {
-                    PlaceholderText("Example (reading): \"I want to rediscover my love for reading and make it part of my daily life again.\"")
-                },
-                keyboardOptions = KeyboardOptions().copy(
-                    imeAction = if (navArgs.editing) ImeAction.Done else ImeAction.Next,
-                    capitalization = KeyboardCapitalization.Sentences
-                ),
-                keyboardActions = KeyboardActions(
-                    onDone = {
-                        if (navArgs.challengeId != null)
+                }
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                // Commitment Field.
+                LimitedCharTextField(
+                    onValueChange = { expectations ->
+                        onAction(
+                            CreateChallengeExpectationsAction.OnExpectationsValueChange(expectations)
+                        )
+                    },
+                    value = state.expectationsInputValue,
+                    maxCharAllowed = CreateChallengeViewModel.MAX_CHALLENGE_EXPECTATIONS_LENGTH,
+                    textError = null,
+                    shape = RoundedCornerShape(16.dp),
+                    placeholder = {
+                        PlaceholderText("Example (reading): \"I want to rediscover my love for reading and make it part of my daily life again.\"")
+                    },
+                    keyboardOptions = KeyboardOptions().copy(
+                        capitalization = KeyboardCapitalization.Sentences,
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                state.expectationUpdateError?.let { message ->
+                    FormErrorText(
+                        message,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+
+                LoadingButton(
+                    onClick = {
+                        if (navArgs.editing && navArgs.challengeId != null)
                             done(
                                 keyboard,
                                 onAction,
                                 challengeId = navArgs.challengeId
                             )
+                        else
+                            goNext(keyboard, onAction)
                     },
-                    onNext = {
-                        goNext(keyboard, onAction)
-                    }
-                ),
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
-
-            Spacer(modifier = Modifier.weight(1f))
-
-            state.expectationUpdateError?.let { message ->
-                FormErrorText(
-                    message,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.padding(16.dp)
+                    enabled = !state.isExpectationUpdateLoading,
+                    isLoading = state.isExpectationUpdateLoading,
+                    label = if (navArgs.editing) "Update" else "Next",
+                    modifier = Modifier
+                        .fillMaxWidth()
                 )
             }
-
-            LoadingButton(
-                onClick = {
-                    if (navArgs.editing && navArgs.challengeId != null)
-                        done(
-                            keyboard,
-                            onAction,
-                            challengeId = navArgs.challengeId
-                        )
-                    else
-                        goNext(keyboard, onAction)
-                },
-                enabled = !state.isExpectationUpdateLoading,
-                isLoading = state.isExpectationUpdateLoading,
-                label = if (navArgs.editing) "Update" else "Next",
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
         }
     }
 }
