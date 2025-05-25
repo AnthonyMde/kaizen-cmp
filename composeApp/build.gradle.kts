4adcc22b80c809fbdc9b1acbc9ad5c4f85ec99ed
AnthonyMde <anthony.mamode@gmail.com>
2025-05-25 20:58:27 +0200

    [realese] Bump android + ios to 1.4.1

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -100,8 +100,8 @@ android {
         applicationId = "com.makapp.kaizen"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 6
-        versionName = "1.4.0"
+        versionCode = 7
+        versionName = "1.4.1"
     }
     buildFeatures {
         buildConfig = true
